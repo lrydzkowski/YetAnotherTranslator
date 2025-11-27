dc60db28c2c81db1de2aa93fe2898eb290b99e3b
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2025-11-27 17:10:38 +0100

    update dependencies

--- src/YetAnotherTranslator.Infrastructure/Persistence/Migrations/TranslatorDbContextModelSnapshot.cs
+++ src/YetAnotherTranslator.Infrastructure/Persistence/Migrations/TranslatorDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace YetAnotherTranslator.Infrastructure.Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.0-rc.2.25502.107")
+                .HasAnnotation("ProductVersion", "10.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
